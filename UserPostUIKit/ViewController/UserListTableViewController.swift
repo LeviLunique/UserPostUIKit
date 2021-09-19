d491b53ce49e403fe736c51fdaaf78739eacd6ee
Levi Lunique <levi.lunique@gmail.com>
2021-09-18 22:13:03 -0400

    Initial Commit - UserListTableViewController

---
+++ UserPostUIKit/ViewController/UserListTableViewController.swift
@@ -0,0 +1,76 @@
+//
+//  UserListTableViewController.swift
+//  UserPostUIKit
+//
+//  Created by user204006 on 9/18/21.
+//
+
+import UIKit
+
+class UserListTableViewController: UITableViewController {
+    
+    private let kBaseURL = "https://jsonplaceholder.typicode.com"
+    
+    private var users = [User]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    // MARK - Ciclo de Vida
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let url = URL(string: "\(kBaseURL)/users") {
+            let session = URLSession(
+                configuration: URLSessionConfiguration.default,
+                delegate: self,
+                delegateQueue: OperationQueue.main)
+            
+            session.dataTask(with: url).resume()
+        }
+        
+    }
+    
+    // MARK - UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultUserCell", for: indexPath)
+        
+        let user = users[indexPath.row]
+        
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.email
+        
+        return cell
+        
+    }
+    
+}
+
+
+extension UserListTableViewController: URLSessionDataDelegate {
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data){
+        if let response = dataTask.response as? HTTPURLResponse,
+           response.statusCode >= 200, response.statusCode < 300 {
+    
+            if let users = try? JSONDecoder().decode([User].self, from: data){
+                self.users = users
+            }
+        }
+    }
+}
